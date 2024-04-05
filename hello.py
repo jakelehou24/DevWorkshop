9b1de254b2d04535d058f708f56e49dfb174b70a
omer18 <omer18@vt.edu>
2024-04-05 12:59:54 -0400

    added contribution of hello.py from omer18

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello, my name is Omer Abbas")
+print("Hello, my name is Jordan Atanassov")
+print("Hello, my name is Jake Lehoullier")
+print("Hello, my name is Reagan Gary")
+print("Hello, my name is Maceo Cardinale Kwik")
+print("Hello, Hello, my name is Omer Abbas")
